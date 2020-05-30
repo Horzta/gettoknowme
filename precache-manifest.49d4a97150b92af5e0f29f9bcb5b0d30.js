3ea72f2f60ef3edff2bfbfe7b2ff99ddf432a4c0
Horzta <horzta@gmail.com>
2020-05-30 19:10:04 +0800

    Updates

---
+++ precache-manifest.49d4a97150b92af5e0f29f9bcb5b0d30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69fa5e59562e92da6aa9a505d06d5a50",
+    "url": "/gettoknowme/index.html"
+  },
+  {
+    "revision": "f7836b7ca99ceeeb8d15",
+    "url": "/gettoknowme/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58eb3dabbb8bbe2d5fe8",
+    "url": "/gettoknowme/static/js/2.3fc8ceac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gettoknowme/static/js/2.3fc8ceac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7836b7ca99ceeeb8d15",
+    "url": "/gettoknowme/static/js/main.75e00f80.chunk.js"
+  },
+  {
+    "revision": "f68e7bf8ec549d1b5296",
+    "url": "/gettoknowme/static/js/runtime-main.f312297f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gettoknowme/static/media/logo.5d5d9eef.svg"
+  }
+]);
